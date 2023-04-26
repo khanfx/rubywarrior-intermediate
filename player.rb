b2835c1bc95bcc093f5726b3fd0a01eb97037029
Fahd Khan <fahd@khanfx.com>
2023-04-26 01:51:42 +0000

    level 2

--- player.rb
+++ player.rb
@@ -1,5 +1,36 @@
+require 'pry-byebug'
+
+MAX_HP = 20
+
 class Player
+
+  def initialize
+    puts '-- Player initialize called --'
+    @turn = 0
+    @minhealth = 20
+    @lasthealth = 20
+  end
+
   def play_turn(warrior)
-    # add your code here
+    @turn += 1
+
+    # Here's what various troubleshooting features look like
+
+    # puts warrior.inspect
+    # puts warrior.to_s
+    # pp warrior
+
+    # if @turn == 35
+    #   binding.pry
+    # end
+
+    # binding.pry
+
+    puts "Results of feel:"
+    pp warrior.feel
+
+    puts "Direction of stairs: #{warrior.direction_of_stairs}"
+
+    warrior.walk! warrior.direction_of_stairs
   end
 end
